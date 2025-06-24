c961ea53a9dea85976dfc22af95e0f361ae7b076
Danny Stark <1ironman19101@gamil.com>
2025-06-24 23:43:07 +0100

    Add Version 1 of NobHUB with AI chats andchats between users working

--- NobHUB/App/auth.py
+++ NobHUB/App/auth.py
@@ -14,31 +14,52 @@ def signup():
         user_password = request.form.get('password0')
         confirm_user_password = request.form.get('password1')
         if user_password!= confirm_user_password:
+            flash("Passwords must be same")
             return redirect(url_for('auth.signup'))
+        elif len(user_password)<=5:
+            flash("Passwords must be greater than 5 digits")
+            return redirect(url_for('auth.signup'))
+        
         username= request.form.get('username')
         user_email= request.form.get('user_email')
         user_number= request.form.get('number')
-        user_password = request.form.get('password0')
-        confirm_user_password = request.form.get('password1')
+    
         user = User.query.filter_by(username=username).first()
+        email = User.query.filter_by(user_email=user_email).first()
+
         if user:
             flash('Username already exist!')
             return redirect(url_for('auth.signup'))
+        if email:
+            flash('Email already registered!')
+            return redirect(url_for('auth.signup'))
+
         user_password_hashed = generate_password_hash(user_password, method='pbkdf2:sha256')
         new_user = User(username=username, user_email=user_email, user_number=user_number,user_password_hash=user_password_hashed,user_image_path='static/images/defaultimg.jpg')
+        nob_ai_exists = User.query.filter_by(username='N.O.B').first()
+        dennis_ai_exists = User.query.filter_by(username='dennis').first()
         nob_ai = User(username='N.O.B', user_number='0000001', user_email='nob@ai.com', user_password_hash='-', user_image_path='static/images/defaultimg.jpg')
         dennis_ai = User(username='Dennis', user_number='0000002', user_email='dennis@ai.com', user_password_hash='-', user_image_path='static/images/defaultimg.jpg')
        
         
         try:
             nob_db.session.add(new_user)
-            nob_db.session.add(nob_ai)
-            nob_db.session.add(dennis_ai)
+            nob_ai_exists = User.query.filter_by(username='N.O.B').first()
+            dennis_ai_exists = User.query.filter_by(username='Dennis').first()
+            if not nob_ai_exists:
+                nob_ai = User(username='N.O.B', user_number='0000001', user_email='nob@ai.com', user_password_hash='-', user_image_path='static/images/defaultimg.jpg')
+                nob_db.session.add(nob_ai)
+            if not dennis_ai_exists:
+                dennis_ai = User(username='Dennis', user_number='0000002', user_email='dennis@ai.com', user_password_hash='-', user_image_path='static/images/defaultimg.jpg')
+                nob_db.session.add(dennis_ai)
+            
+           
             nob_db.session.commit()
             return redirect('/login')
         except Exception as e:
+            nob_db.session.rollback()
             print(e)
-            return "Error 101: Failed to add user"    
+            return "Error 101: Failed to add user. Please try again!"    
     else:
         return render_template('signup.html')
 
