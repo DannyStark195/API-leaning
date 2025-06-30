008f4af376d627b30c7fc7c474b1ea9e0087caa2
Danny Stark <1ironman19101@gamil.com>
2025-06-30 23:12:08 +0100

    added WTF in routes and login page

--- NobHUB/App/auth.py
+++ NobHUB/App/auth.py
@@ -5,71 +5,94 @@ from .NOB_AI import NOB
 from sqlalchemy import or_, and_
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email
 
 #The auth.py defines the routes and logic for registration and authentication of users
 auth = Blueprint('auth', __name__)
-
+#Flask WTF
+class SignupForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired("Username is required")])
+    email = StringField("Email"), validators=[DataRequired("Email is required"),Email()]
+    phoneNumber = StringField("Phone Number", validators=[DataRequired("Phone Number is required")])
+    password0 = PasswordField("Password", validators=[DataRequired("Password is required")])
+    password1 = PasswordField("Password", validators=[DataRequired("Password is required")])
+    submit = SubmitField("Save")
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(message="Username is required")])
+    user_password = PasswordField("Password", validators=[DataRequired("Password is required")])
+    submit = SubmitField("GO")
 @auth.route('/signup', methods=['POST', 'GET'])
 def signup():
+    signup_form = SignupForm()
     if request.method == 'POST':
-        user_password = request.form.get('password0')
-        confirm_user_password = request.form.get('password1')
-        if user_password!= confirm_user_password:
-            flash("Passwords must be same")
-            return redirect(url_for('auth.signup'))
-        elif len(user_password)<=5:
-            flash("Passwords must be greater than 5 digits")
-            return redirect(url_for('auth.signup'))
-        
-        username= request.form.get('username')
-        user_email= request.form.get('user_email')
-        user_number= request.form.get('number')
-    
-        user = User.query.filter_by(username=username).first()
-        email = User.query.filter_by(user_email=user_email).first()
+        if signup_form.validate_on_submit():
 
-        if user:
-            flash('Username already exist!')
-            return redirect(url_for('auth.signup'))
-        if email:
-            flash('Email already registered!')
-            return redirect(url_for('auth.signup'))
-
-        user_password_hashed = generate_password_hash(user_password, method='pbkdf2:sha256')
-        new_user = User(username=username, user_email=user_email, user_number=user_number,user_password_hash=user_password_hashed,user_image_path='static/images/defaultimg.jpg')
-        nob_ai_exists = User.query.filter_by(username='N.O.B').first()
-        dennis_ai_exists = User.query.filter_by(username='dennis').first() 
-        
-        try:
-            nob_db.session.add(new_user)
-            nob_ai_exists = User.query.filter_by(username='N.O.B').first()
-            dennis_ai_exists = User.query.filter_by(username='Dennis').first()
-            if not nob_ai_exists:
-                nob_ai = User(username='N.O.B', user_number='0000001', user_email='nob@ai.com', user_password_hash='-', user_image_path='static/images/defaultimg.jpg')
-                nob_db.session.add(nob_ai)
-            if not dennis_ai_exists:
-                dennis_ai = User(username='Dennis', user_number='0000002', user_email='dennis@ai.com', user_password_hash='-', user_image_path='static/images/defaultimg.jpg')
-                nob_db.session.add(dennis_ai)
+            user_password = signup_form.password0.data
+            confirm_user_password = signup_form.password1.data
+            if user_password!= confirm_user_password:
+                flash("Passwords must be same")
+                return redirect(url_for('auth.signup'))
+            elif len(user_password)<=5:
+                flash("Passwords must be greater than 5 digits")
+                return redirect(url_for('auth.signup'))
             
-           
-            nob_db.session.commit()
-            return redirect('/login')
-        except Exception as e:
-            nob_db.session.rollback()
-            print(e)
-            return "Error 101: Failed to add user. Please try again!"    
+            username= signup_form.username.data
+            user_email= signup_form.email.data
+            user_number= signup_form.phoneNumber.data
+
+            user = User.query.filter_by(username=username).first()
+            email = User.query.filter_by(user_email=user_email).first()
+
+            if user:
+                flash('Username already exist!')
+                return redirect(url_for('auth.signup'))
+            if email:
+                flash('Email already registered!')
+                return redirect(url_for('auth.signup'))
+
+            user_password_hashed = generate_password_hash(user_password, method='pbkdf2:sha256')
+            new_user = User(username=username, user_email=user_email, user_number=user_number,user_password_hash=user_password_hashed,user_image_path='static/images/defaultimg.jpg')
+            nob_ai_exists = User.query.filter_by(username='N.O.B').first()
+            dennis_ai_exists = User.query.filter_by(username='dennis').first() 
+            
+            try:
+                nob_db.session.add(new_user)
+                nob_ai_exists = User.query.filter_by(username='N.O.B').first()
+                dennis_ai_exists = User.query.filter_by(username='Dennis').first()
+                if not nob_ai_exists:
+                    nob_ai = User(username='N.O.B', user_number='0000001', user_email='nob@ai.com', user_password_hash='-', user_image_path='static/images/defaultimg.jpg')
+                    nob_db.session.add(nob_ai)
+                if not dennis_ai_exists:
+                    dennis_ai = User(username='Dennis', user_number='0000002', user_email='dennis@ai.com', user_password_hash='-', user_image_path='static/images/defaultimg.jpg')
+                    nob_db.session.add(dennis_ai)
+                
+            
+                nob_db.session.commit()
+                return redirect('/login')
+            except Exception as e:
+                nob_db.session.rollback()
+                print(e)
+                return "Error 101: Failed to add user. Please try again!"    
     else:
         return render_template('signup.html')
 
 @auth.route('/login')
 def login():
-    return render_template('login.html')
+    login_form = LoginForm()
+    return render_template('login.html', login_form=login_form)
 
 @auth.route('/login', methods=['POST'])
 def login_post():
-    username = request.form.get('username')
-    user_password = request.form.get('password0')
-
+    # username = request.form.get('username')
+    # user_password = request.form.get('password0')
+    login_form = LoginForm()
+    username = None
+    user_password = None
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        user_password = login_form.user_password.data
     user_or_email = User.query.filter(or_(User.username==username, User.user_email==username)).first()
     
     if not user_or_email:
